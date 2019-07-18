08ac8fd436e70275f3243956788c71d43896e3c7
Rubén Segura <rsegrom@gmail.com>
2019-07-18 11:46:10 +0200

    filtro tareas terminadas o pendientes

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { TareasService } from '../../services/tareas.service';
 import { Lista } from 'src/app/models/lista.model';
+import { IonList } from '@ionic/angular';
+
 
 
 @Component({
@@ -11,17 +13,20 @@ import { Lista } from 'src/app/models/lista.model';
 })
 export class ListasComponent {
 
+  @ViewChild( IonList ) lista: IonList;
+  @Input() terminada = true;
+
   constructor( public tareasService: TareasService, private router: Router ) { 
     console.log('-- Servicio inicializado --')
   }
 
-listaSeleccionada( lista: Lista ) {
-  const idLista = lista.id;
-  this.router.navigateByUrl(`${ this.router.url }/agregar/${ idLista }`);
-}
+  listaSeleccionada( lista: Lista ) {
+    const idLista = lista.id;
+    this.router.navigateByUrl(`${ this.router.url }/agregar/${ idLista }`);
+  }
 
-borrarLista( lista: Lista ) {
-  this.tareasService.borrarLista( lista );
-}
+  borrarLista( lista: Lista ) {
+    this.tareasService.borrarLista( lista );
+  }
 
 }
